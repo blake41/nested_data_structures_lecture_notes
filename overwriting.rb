fc564b03e9566872cbd6059d37767769c950d76e
Blake Johnson <blake41@gmail.com>
2014-05-07 13:31:14 -0400

    notes

---
+++ overwriting.rb
@@ -0,0 +1,9 @@
+require 'pp'
+# a = [{:name => {:first => "blake", :last => "johnson"}}, {:name => {:first => "ashley", :last => "williams"}}]
+# a.each do |person|
+#   person.each_pair do |key, person; my_var|
+#     person[:new_key] = "wat"
+#   end
+# end
+
+# pp a
---
+++ word_count.rb
@@ -0,0 +1,23 @@
+require 'pp'
+
+# user a heredoc
+sentence = <<-something
+    Steven likes the movies.  Blake likes to ride his bike but hates movies.
+    Blake is taller than steven.  Steven is a great teacher.
+  something
+
+# END GOAL - Find out the cost of all words that appear the most frequently in a sentence.
+# Assume the letter A costs 1, B costs 2, C costs 3
+
+# count frequency of words in sentence
+# how do i know what i'll get yielded when i call each_pair?
+
+# group by frequency
+
+# find all words that appear the most times in the sentence
+
+# build a cost of each letter hash
+
+# calculate the cost of each word
+
+# sum the cost of each word
